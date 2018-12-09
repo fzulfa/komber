b0e3374c66e37a7c902778ae7c9b8dbf3be73939
fzulfa <32604347+fzulfa@users.noreply.github.com>
2018-12-09 21:51:16 +0700

    Add files via upload

---
+++ komber.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v8.6 Beta2
+MySQL - 5.5.5-10.1.29-MariaDB : Database - komber
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`komber` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `komber`;
+
+/*Table structure for table `potholes` */
+
+DROP TABLE IF EXISTS `potholes`;
+
+CREATE TABLE `potholes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lokasi` varchar(100) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `potholes` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
