0d70dd13028a62ce9272d37bc24c9b1ea779f531
Astrid Febrianca <astridstr24@gmail.com>
2018-12-09 22:13:12 +0700

    ui komber

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,23 @@
+
+    <!-- Logo -->
+    <a href="../../index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>PD</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">Pothole </span>
+    </a>
+
+<nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+        </ul>
+      </div>
+    </nav>
